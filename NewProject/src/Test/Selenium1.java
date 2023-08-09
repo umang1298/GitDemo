684be59260f849d6011ad39ddecb8023b8852ffd
umang.tamrakar <umangtamrakar12@hmail.com>
2023-08-10 00:32:50 +0530

    first commit

---
+++ NewProject/src/Test/Selenium1.java
@@ -0,0 +1,16 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Umang\\New folder\\chromedriver.exe");  
+	    WebDriver driver =new ChromeDriver();
+
+	}
+
+}
